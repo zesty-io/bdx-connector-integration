2a82b68c3c64280d3034ccf98c0325b5cefd9eb8
Wei-En Wong <wei-en.wong@zesty.io>
2019-04-10 17:32:04 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const parseString = require('xml2js').parseString;
+
+require("dotenv").config()
+
+const server = require("express")();
+const zesty = require("@zesty-io/sdk-express-middleware");
+
+const parseLib = require("./lib/parse")
+
+const bodyParser = require("body-parser");
+
+const fs = require("fs")
+
+server.use(
+    zesty({
+        instance: process.env.ZESTY_INSTANCE_ZUID,
+        token: process.env.ZESTY_INSTANCE_TOKEN,
+        email: process.env.ZESTY_USER_EMAIL,
+        password: process.env.ZESTY_USER_PASSWORD,
+        options: {
+            authURL: process.env.ZESTY_AUTH_API,
+            instancesAPIURL: process.env.ZESTY_INSTANCE_API,
+            accountsAPIURL: process.env.ZESTY_ACCOUNTS_API
+        }
+    })
+);
+
+server.use(bodyParser.json())
+
+server.get("/:contentModelZUID/items",  async (req, res) => {
+    const items = await req.app.locals.zesty.instance.getItems(req.params.contentModelZUID)
+    res.send(items.data)
+})
+
+server.post("/createItem", async (req, res) => {
+    try {
+        const xml = `<title>Hello xml2js!</title>`;
+        const xmlToJSON = await parseLib.parse(xml);
+        console.log(xmlToJSON)
+        const modelZUID = "6-61f43c-wbmxb6";
+
+        const payload = {
+            web: {
+                name: "Test Dataset Content Model 9000+",
+                label: "Test Dataset",
+                type: "dataset",
+                parentZUID: "0",
+                listed: true,
+                pathPart: "test-dataset-contentmodel-90001",
+            },
+            data: xmlToJSON
+        }
+
+        const result = await req.app.locals.zesty.instance.createItem(modelZUID, payload)
+        res.send(result)
+    } catch (error) {
+        console.error(error);
+        res.send(error)
+    }
+})
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+})
+
+fs.readFile("./planspec.xml", (err, data) => {
+    if (err) throw err;
+    parseLib.parse(data).then(output => {
+        console.log(output.Builders.Corporation)
+    });
+    
+})
+
+
