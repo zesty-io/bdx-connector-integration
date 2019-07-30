ad94cfd4eb8aa0ebff34acd88c643cfeffa08c85
Randy Apuzzo <apuzzo@gmail.com>
2019-07-30 10:08:22 -0700

    mapping bdx

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ exports.bdxIntegration = (req, res) => {
 }
 
 const exportBDXIntegration = async (req, res) => {
-    console.log('running')
+    //console.log('running')
     const client = new ftp.Client()
     client.ftp.verbose = false
     try {
@@ -44,7 +44,67 @@ const exportBDXIntegration = async (req, res) => {
         var parsedJSON = convert.xml2js(xml, options);
         //parsedJSON.Builders.Corporation.Builder.Subdivision.SubImage // array
         //parsedJSON.Builders.Corporation.Builder.Subdivision.Plan // array
-        res.send(parsedJSON.Builders.Corporation.Builder.Subdivision.Plan); //parsedJSON.Builders.Corporation.Builder.Subdivision.SubImage.Plan);
+        let plansArray = parsedJSON.Builders.Corporation.Builder.Subdivision.Plan
+        let preparedPlans = []
+        
+
+
+        for (index = 0; index < plansArray.length; index++) { 
+            
+            let o = plansArray[index]
+            //console.log(o.PlanName._text); 
+            let mainImage, floorPlanImage1, floorPlanImage2 = ""
+
+            if (o.hasOwnProperty('PlanImages')){
+                mainImage = o.PlanImages.hasOwnProperty('ElevationImage') ? o.PlanImages.ElevationImage._text : ""
+                if(Array.isArray(o.PlanImages.ElevationImage)){
+                    mainImage = o.PlanImages.ElevationImage[0]._text
+                }
+                if(Array.isArray(o.PlanImages.FloorPlanImage)){
+                    floorPlanImage1 = o.PlanImages.FloorPlanImage[0]._text
+                    floorPlanImage2 = o.PlanImages.FloorPlanImage[1]._text
+                }
+            }
+
+            let halfBaths = o.hasOwnProperty('HalfBaths') ? o.HalfBaths._text : 0;
+            let stories = o.hasOwnProperty('Stories') ? o.Stories._text : 1;
+            if(o.hasOwnProperty('Spec')){
+                halfBaths = o.Spec.hasOwnProperty('SpecHalfBaths') ? o.Spec.SpecHalfBaths._text : halfBaths;
+                stories = o.Spec.hasOwnProperty('SpecStories') ?  o.Spec.SpecStories._text : stories;
+            }
+           
+
+            let planObject = {
+                'plan_id': o._attributes.PlanID,
+                'plan_name' : o.PlanName._text,
+                'marketing_headline' : o.MarketingHeadline._text,
+                'main_image' :mainImage,
+                'base_price': Math.round(o.BasePrice._text),
+                'bedrooms': o.Bedrooms._text,
+                'baths': o.Baths._text,
+                'half_baths': halfBaths,
+                'garage': o.Garage._text,
+                'stories': stories,
+                'square_footage' : o.BaseSqft._text,
+                'builder' : parsedJSON.Builders.Corporation.Builder.BrandName._text,
+                'plan_type': unCamelCase(o._attributes.Type),
+                'floor_plan_1': floorPlanImage1,
+                'floor_plan_2': floorPlanImage2,
+                'description' : o.Description._cdata
+
+            }
+            let apiPostBody = {
+                'content': planObject,
+                'meta': {
+                    'path_part': slugify(planObject.plan_name)
+                }
+            }
+
+            preparedPlans.push(apiPostBody)
+        } 
+
+
+        res.send(preparedPlans) //parsedJSON.Builders.Corporation.Builder.Subdivision.Plan); //parsedJSON.Builders.Corporation.Builder.Subdivision.SubImage.Plan);
 
         //await client.upload(fs.createReadStream("README.md"), "README.md")
     }
@@ -54,6 +114,26 @@ const exportBDXIntegration = async (req, res) => {
     
 }
 
+
+function unCamelCase(str){
+  str = str.replace(/([a-z\xE0-\xFF])([A-Z\xC0\xDF])/g, '$1 $2');
+  return str;
+}
+
+function slugify(string) {
+  const a = 'àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·/_,:;'
+  const b = 'aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string.toString().toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
 
 // const zestyLogin = async (email, password) => {
 //     return new Promise((resolve, reject) => {
