dd3bc73d32b1399e7e47c0919856c255fc146268
Randy Apuzzo <apuzzo@gmail.com>
2019-07-30 00:06:40 -0700

    read bdx integration, getting plans ready for loop

---
+++ README.md
@@ -0,0 +1,63 @@
+# BDX Integration
+
+Makes an FTP connection to a customers BDX ftp account, pulls the XML files, and imports them into a specific content model.
+
+Post request, requires 
+ftp host
+instanceZUID
+
+
+**Endpoints**
+
+- zesty-dev: https://us-central1-zesty-dev.cloudfunctions.net/bdxIntegration
+- zesty-stage: https://us-central1-zesty-stage.cloudfunctions.net/bdxIntegration
+- zesty-prod: https://us-central1-zesty-prod.cloudfunctions.net/bdxIntegration
+
+# local deployment
+
+install functions (google's test package)
+
+
+
+
+### Testing Remote Project
+
+Provide a Project ID as shown below before starting the Emulator:
+
+```
+functions config set projectId zesty-dev
+```
+
+Before you can deploy a function, you need to start the Emulator as follows:
+
+```
+functions start
+```
+
+You stop the Emulator by calling stop:
+
+```
+functions stop
+```
+
+Deploy an HTTP function to the Emulator as follows:
+
+```
+functions deploy bdxIntegration --env-vars-file .env.yaml --trigger-http --timeout=240s
+```
+
+Read the last 50 lines of the log from the Emulator:
+
+```
+functions logs read --limit=50
+```
+
+
+## Deployment
+
+Note: for local developer you need to paste in values, remove before commits.
+
+- Local: `npm run deploy:dev`
+- Dev: `npm run deploy:dev`
+- Stage: `npm run deploy:stage`
+- Prod: `npm run deploy:prod`
--- index.js
+++ index.js
@@ -1,59 +1,152 @@
-require("dotenv").config()
 
-const server = require("express")();
-const zesty = require("@zesty-io/sdk-express-middleware");
-
+const ftp = require("basic-ftp")
 const parseLib = require("./lib/parse")
-
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser")
+const request = require('request')
+const zestyAPI = require('zestyio-api-wrapper')
 
 const fs = require("fs")
+require('env-yaml').config()
 
-server.use(
-    zesty({
-        instance: process.env.ZESTY_INSTANCE_ZUID,
-        token: process.env.ZESTY_INSTANCE_TOKEN,
-        email: process.env.ZESTY_USER_EMAIL,
-        password: process.env.ZESTY_USER_PASSWORD,
-        options: {
-            authURL: process.env.ZESTY_AUTH_API,
-            instancesAPIURL: process.env.ZESTY_INSTANCE_API,
-            accountsAPIURL: process.env.ZESTY_ACCOUNTS_API
-        }
-    })
-);
+exports.bdxIntegration = (req, res) => {
+  const cors = require('cors')()
+    console.log('running')
+  cors(req, res, () => {
+    exportBDXIntegration(req, res)
+  })
+}
 
-server.get("/:contentModelZUID/items",  async (req, res) => {
+const exportBDXIntegration = async (req, res) => {
+    console.log('running')
+    const client = new ftp.Client()
+    client.ftp.verbose = false
     try {
-        // Get XML file via FTP
-        // Parse XML file
-        // Build Payload
-        // Send Payload to instanceß
+        await client.access({
+            host: process.env.FTPHOST,
+            user: process.env.FTPUSER,
+            password: process.env.FTPPASS,
+            secure: false
+        })
+        let list = await client.list()
+        
+       // res.json(list)
+        let writableStream = ""
+        let remoteFilename = list[0].name;
+         
+        await client.download(fs.createWriteStream("/tmp/file.xml"), remoteFilename).catch(err => {
+            console.log(err.toString());
+        })
+        client.close() // close ftp connection
+       
+        var convert = require('xml-js');
+        var xml = require('fs').readFileSync('/tmp/file.xml', 'utf8');
+        var options = {compact: true};
+        var parsedJSON = convert.xml2js(xml, options);
+        //parsedJSON.Builders.Corporation.Builder.Subdivision.SubImage // array
+        //parsedJSON.Builders.Corporation.Builder.Subdivision.Plan // array
+        res.send(parsedJSON.Builders.Corporation.Builder.Subdivision.Plan); //parsedJSON.Builders.Corporation.Builder.Subdivision.SubImage.Plan);
 
-        const xml = `<title>Hello xml2js!</title>`;
-        const xmlToJSON = await parseLib.parse(xml);
-        console.log(xmlToJSON)
-
-        const payload = {
-            web: {
-                name: "Test Dataset Content Model 9000+",
-                label: "Test Dataset",
-                type: "dataset",
-                parentZUID: "0",
-                listed: true,
-                pathPart: "test-dataset-contentmodel-90001",
-            },
-            data: xmlToJSON
-        }
-
-        const result = await req.app.locals.zesty.instance.createItem(modelZUID, payload)
-        res.send(result)
-    } catch (error) {
-        console.error(error);
-        res.send(error)
+        //await client.upload(fs.createReadStream("README.md"), "README.md")
     }
-})
+    catch(err) {
+        console.log(err)
+    }
+    
+}
 
-server.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
-})
+
+// const zestyLogin = async (email, password) => {
+//     return new Promise((resolve, reject) => {
+//         request.post({
+//             url: 'https://svc.zesty.io/auth/login',
+//             formData: {
+//                 email,
+//                 password
+//             },
+//             json: true
+//         }, (error, response, body) => {
+//             if (error) {
+//                 return reject({
+//                     errorCode: -1,
+//                     errorMessage: 'Unexpected error.'
+//                 })
+//             }
+
+//             if (response.statusCode !== 200) {
+//                 return reject({
+//                     errorCode: response.statusCode,
+//                     errorMessage: body.message || ''
+//                 })
+//             }
+
+//             return resolve(body.meta.token)
+//         })
+//     })
+// }
+
+// const zestyVerifyToken = async(token) => {
+//     return new Promise((resolve, reject) => {
+//         request.get({
+//             url: 'https://svc.zesty.io/auth/verify',
+//             headers: {
+//                 Authorization: `Bearer ${token}`
+//             },
+//             json: true
+//         }, (error, response, body) => {
+//             if (error) {
+//                 return reject({
+//                     errorCode: -1,
+//                     errorMessage: 'Unexpected error.'
+//                 })
+//             }
+
+//             if (response.statusCode !== 200) {
+//                 return resolve(false)
+//             }
+
+//             return resolve(true)
+//         })
+//     })
+// }
+
+// const goZesty = async () => {
+//     // Get user email address and password...
+//     const email = process.env.ZESTY_USER_EMAIL_ADDRESS
+//     const password = process.env.ZESTY_USER_PASSWORD
+
+//     // Get the instance ZUID to work with
+//     const instanceZUID = process.env.ZESTY_INSTANCE_ZUID
+
+//     try {
+//         // Attempt to login and retrieve access token
+//         let zestyToken = await zestyLogin(email, password)
+
+//         // Try verifying token, expect true
+//         let isValidToken = await zestyVerifyToken(zestyToken)
+//         console.log(`Token is ${isValidToken ? '' : 'not '}valid.`)
+
+//         // Instantiate API and test it
+//         // const zesty = new zestyAPI(instanceZUID, zestyToken)
+//         // const views = await zesty.getViews()
+
+//         //console.log(views)
+//     } catch (ex) {
+//         console.error('Error accessing zesty.io:')
+//         console.error(ex)
+//     }
+// }
+
+//goZesty()
+
+// zesty({
+//         instance: process.env.ZESTY_INSTANCE_ZUID,
+//         token: process.env.,
+//         email: process.env.ZESTY_USER_EMAIL,
+//         password: process.env.ZESTY_USER_PASSWORD,
+//         options: {
+//             authURL: process.env.ZESTY_AUTH_API,
+//             instancesAPIURL: process.env.ZESTY_INSTANCE_API,
+//             accountsAPIURL: process.env.ZESTY_ACCOUNTS_API
+//         }
+//     })
+
